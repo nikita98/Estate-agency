e6fba7e7eb8b50234a5844703a512d0fda329126
Nikita Zuev <nikita_zuev_98@mail.ru>
2019-08-29 00:49:51 +0800

    users

---
+++ pages/users/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-list>
+    <v-subheader>REPORTS</v-subheader>
+    <v-list-item-group color="primary">
+      <v-list-item v-for="(user, i) in users" :key="i" @click="openUser(user)">
+        <v-list-item-avatar>
+          <v-img :src="user.avatar"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-html="user.name"></v-list-item-title>
+          <v-list-item-subtitle class v-html="user.lastlogin"></v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+export default {
+  async fetch({ store }) {
+    if (store.getters["users/users"].length === 0) {
+      await store.dispatch("users/fetch");
+    }
+  },
+  computed: {
+    users() {
+      return this.$store.getters["users/users"];
+    }
+  },
+  methods: {
+    openUser(user) {
+      this.$router.push("/users/" + user.id);
+    }
+  }
+};
+</script>
+
+
