3931de3a26888671feefd5875cd78ce6703eeccd
Nikita Zuev <nikita_zuev_98@mail.ru>
2019-08-28 14:36:22 +0800

    Vs code

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,18 +32,6 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn
         icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
         @click.stop="fixed = !fixed"
       >
         <v-icon>mdi-minus</v-icon>
